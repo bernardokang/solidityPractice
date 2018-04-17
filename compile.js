f86189285ab7fc73a554567e57a66d36f7471cc6
bernardokang <applepath@icloud.com>
2018-04-17 20:53:33 +0900

    compilejs

--- compile.js
+++ compile.js
@@ -2,10 +2,10 @@ const path = require('path');
 const fs = require('fs'); // fs=file system
 const solc = require('solc');
 
-const lottoPath = path.resolve(__dirname, 'contracts', 'inbox.sol');
+const inboxPath = path.resolve(__dirname, 'contracts', 'inbox.sol');
 //__dirname 커런트 작업폴더옵션, 'contracts'라는 폴더의 'Inbox.sol'를 셋팅
 
-const source = fs.readFileSync(lottoPath, 'utf8');
+const source = fs.readFileSync(inboxPath, 'utf8');
 // 인코딩 옵션
 
 // console.log(solc.compile(source, 1));
